d2f2857d35558991bc2dde76b8dec22b9939fdc3
shaversj <shaversj@gmail.com>
2020-12-03 08:15:50 -0500

    Day 3 Solutions

---
+++ 03/solution.py
@@ -0,0 +1,36 @@
+def search_map(area_map, right_amount, down_amount):
+    row = 0
+    column = right_amount
+    num_of_trees = 0
+    max_rows = len(area_map)
+    while row != max_rows - 1:
+        row += down_amount
+        if area_map[row][column] == "#":
+            num_of_trees += 1
+
+        column += right_amount
+
+    print(f"Number of Trees: {num_of_trees}")
+
+    return num_of_trees
+
+
+def load_map(filename):
+    board = []
+    with open(filename) as f:
+        data = f.readlines()
+
+    for line in data:
+        l = line.strip()
+        long_line = l * 300
+        board.append(long_line + "\n")
+
+    return board
+
+
+large_map = load_map("input.txt")
+search_map(large_map, 1, 1)
+search_map(large_map, 3, 1)
+search_map(large_map, 5, 1)
+search_map(large_map, 7, 1)
+search_map(large_map, 1, 2)
