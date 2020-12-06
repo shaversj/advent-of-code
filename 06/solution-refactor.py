b52379191ef5b32996c194d69490bc633ab3b16d
shaversj <shaversj@gmail.com>
2020-12-06 09:16:45 -0500

    Day 6 Solutions

---
+++ 06/solution-refactor.py
@@ -0,0 +1,49 @@
+def load_questions_part_1(filename):
+    with open(filename) as f:
+        answers = set()
+        sum_of_answered_questions = 0
+        for line in f:
+            if line == "\n":
+                sum_of_answered_questions += len(answers)
+                answers.clear()
+                continue
+            else:
+                split_line = list(line.strip())
+                for letter in split_line:
+                    answers.add(letter)
+
+        sum_of_answered_questions += len(answers)
+        print(sum_of_answered_questions)
+
+
+def load_questions_part_2(filename):
+
+    with open(filename) as f:
+        answers = {}
+        sum_of_answered_questions = 0
+        num_of_people_in_group = 0
+        for line in f:
+            if line == "\n":
+                for k, v in answers.items():
+                    if v == num_of_people_in_group:
+                        sum_of_answered_questions += 1
+                answers.clear()
+                num_of_people_in_group = 0
+                continue
+            else:
+                num_of_people_in_group += 1
+                split_line = list(line.strip())
+                for letter in split_line:
+                    if letter in answers:
+                        answers[letter] += 1
+                    else:
+                        answers[letter] = 1
+
+        for k, v in answers.items():
+            if v == num_of_people_in_group:
+                sum_of_answered_questions += 1
+
+        print(sum_of_answered_questions)
+
+
+load_questions_part_2("input.txt")
