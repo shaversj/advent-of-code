c213fb222b28382f4c3447a729385218d45846b0
shaversj <shaversj@gmail.com>
2021-01-14 18:45:38 -0500

    Day 12 Solution Attempt

--- 2020/12/solution.py
+++ 2020/12/solution.py
@@ -17,20 +17,62 @@ def process_actions(actions):
         value = int(command[1:])
 
         if action == "F":
+            print(action, value)
 
-            directions[current_direction] += abs(directions[compass_map[current_direction]] - value)
-            if directions[compass_map[current_direction]] - value < 0:
-                directions[compass_map[current_direction]] = 0
+            opposite_pointing_direction = compass_map[current_direction]
+            opposite_pointing_direction_value = directions[compass_map[current_direction]]
+
+            if opposite_pointing_direction_value == 0:
+                directions[current_direction] += value
+
+            elif opposite_pointing_direction_value - value > 0:
+                directions[opposite_pointing_direction] = opposite_pointing_direction_value - value
+
+                directions[current_direction] += value
             else:
-                directions[compass_map[current_direction]] = directions[compass_map[current_direction]] - value
+                directions[opposite_pointing_direction] = 0
+                directions[current_direction] += abs(opposite_pointing_direction_value - value)
+
+            print(directions)
+
+            # directions[current_direction] += abs(directions[compass_map[current_direction]] - value)
+            # if directions[compass_map[current_direction]] - value < 0:
+            #     directions[compass_map[current_direction]] = 0
+            # else:
+            #     directions[compass_map[current_direction]] = directions[compass_map[current_direction]] - value
+            # print(directions)
 
         elif action in ["N", "S", "E", "W"]:
-            directions[action] += abs(directions[compass_map[action]] - value)
-            if directions[compass_map[action]] - value < 0:
-                directions[compass_map[action]] = 0
+            print(action, value)
+            opposite_direction = compass_map[action]
+            opposite_direction_value = directions[opposite_direction]
+
+            if opposite_direction_value == 0:
+                directions[action] += value
+
+            elif opposite_direction_value - value > 0:
+                # Subtract value from opposite direction
+                directions[opposite_direction] = opposite_direction_value - value
+
+                # Add value to current direction
+                directions[action] += value
             else:
-                directions[compass_map[action]] = directions[compass_map[action]] - value
+                # Update opposite direction to 0
+                directions[opposite_direction] = 0
+
+                # Update curr direction with abs value
+                directions[action] += abs(opposite_direction_value - value)
+
             print(directions)
+            # # Updates direction
+            # directions[action] += abs(directions[compass_map[action]] - value)
+            # print(directions)
+            # if directions[compass_map[action]] - value < 0:
+            #     directions[compass_map[action]] = 0
+            # else:
+            #     # Updates corresponding direction
+            #     directions[compass_map[action]] = directions[compass_map[action]] - value
+            # #print(directions)
         else:
             print(action, value)
             print(current_direction)
@@ -54,7 +96,7 @@ def process_actions(actions):
         # if action == "F":
         #     directions[current_direction] += value - directions[current_direction]
     print(directions)
-    print((abs(directions["N"] - directions["S"]) + (directions["E"] - directions["W"])))
+    print(((abs(directions["N"] - directions["S"])) + abs(directions["E"] - directions["W"])))
 
 
 def change_direction(turn, degrees):
@@ -66,3 +108,5 @@ def change_direction(turn, degrees):
 
 actions_from_file = load_actions("input.txt")
 process_actions(actions_from_file)
+
+# 195931632882400
